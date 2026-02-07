23f8112122849ac3034d4bb0415855fba5e21ba6
Virgilius Nana Manek <virgilius.nana.manek0719@gmail.com>
2026-02-07 18:15:46 +0700

    feat: define rate limit type

---
+++ ratelimit.go
@@ -0,0 +1,19 @@
+package ratelimit
+
+import (
+	"context"
+	"time"
+)
+
+
+type Result struct {
+	Allowed    bool
+	Remaining  int
+	Limit      int
+	ResetAfter time.Duration
+}
+
+
+type Limiter interface {
+	Allow(ctx context.Context, key string) (*Result, error)
+}
